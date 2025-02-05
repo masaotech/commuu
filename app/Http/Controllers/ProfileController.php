6db33b9b65865af519fd45ad988ded6a399fe8fc
masaotech <okada85m@yahoo.co.jp>
2025-02-05 22:52:39 +0900

    アカウント削除時に所属グループの唯一の管理者の場合、メッセージ表示

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileUpdateRequest;
+use App\Http\Requests\Profile\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use App\Models\User;
+use App\Models\GroupUser;
+use App\Consts\UserRoleConst;
 
 class ProfileController extends Controller
 {
@@ -48,6 +49,22 @@ class ProfileController extends Controller
         ]);
 
         $user = $request->user();
+
+        // アカウント削除前にグループ削除の確認
+        // 所属するグループ（管理者権限）一覧の取得
+        $userBelongGroups = GroupUser::where('user_id', '=', $user->id)
+            ->where('user_role_id', '=', UserRoleConst::ADMIN)->get();
+        foreach ($userBelongGroups as $userBelongGroup) {
+            // 対象グループの監視者権限メンバー一覧の取得
+            $groupMembers = GroupUser::where('group_id', '=', $userBelongGroup->group_id)
+                ->where('user_role_id', '=', UserRoleConst::ADMIN)->get();
+            if ($groupMembers->count() === 1) {
+                // 1件のみの場合（削除するアカウントがそのグループの唯一の管理者であるためエラー）
+                $message = '「' . $groupMembers->first()->group->name . '」　はあなたが唯一の管理者であるグループです。' .
+                    'アカウント削除前にグループの削除もしくは、他メンバーに管理者権限を設定してください。';
+                return Redirect::to('/profile')->with('flash-message-error', $message);
+            }
+        }
 
         Auth::logout();
 
